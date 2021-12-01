d841d43bec94ba2d030afb16020c5654c9a5f99d
Ngaicode <Mjombakevin376@gmail.com>
2021-12-01 21:22:03 +0300

    learning truthiness and falsiness

---
+++ truthiness.py
@@ -0,0 +1,5 @@
+animal = input("enter your favorite animal\n")
+if animal:#when user inputs anything,it'll make this condition true hence the block of indented code will be run
+    print(animal + " is my favorite too")
+else:#if user doesn't input anything ,itll make this condition true hence this will execute telling the user that they need to put input
+    print("you didn't say anything")    
