de6cc1dcf5bf8e8d7c0eb7abf6f29c81a1503a9c
Ngaicode <Mjombakevin376@gmail.com>
2021-12-01 12:28:05 +0300

    getting user input

---
+++ userinput.py
@@ -0,0 +1,5 @@
+
+age = input("how old are you ?")#the input function allows us to get user input which is then assigned to the age variable
+age = int(age) #the int function allows us to change the user data into int data type
+print(f"okay ,you said you are {age} years old") #the f format allows us to mix different data types into a string
+#im happy i could remember this much
