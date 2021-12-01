3e55d5089972928fcfaff34dea18fc4400edae1e
Ngaicode <Mjombakevin376@gmail.com>
2021-12-01 21:25:17 +0300

    add extra info in comments

--- truthiness.py
+++ truthiness.py
@@ -2,4 +2,7 @@ animal = input("enter your favorite animal\n")
 if animal:#when user inputs anything,it'll make this condition true hence the block of indented code will be run
     print(animal + " is my favorite too")
 else:#if user doesn't input anything ,itll make this condition true hence this will execute telling the user that they need to put input
-    print("you didn't say anything")    
+    print("you didn't say anything")  
+
+#empty strings,the number zero and the python value None are all inherently falsey 
+# #evrything else is inherently truthy      
