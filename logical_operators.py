2079d5e84232da59be2a2dbc998f2668b6c90143
Ngaicode <Mjombakevin376@gmail.com>
2021-12-01 23:16:46 +0300

    and or logical operators

--- logical_operators.py
+++ logical_operators.py
@@ -2,4 +2,6 @@ city = input("where do you live? \n")
 if city == "los angeles" or city == "san fransisco":
     print("you live in Califonia")
 else:
-    print("you live elsewhere")    
+    print("you live elsewhere")   
+    # in the case of 'or' only one condition has to be met
+    #in the case of 'and' ,both conditions have to be met
